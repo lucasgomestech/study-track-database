e87a65ecfdbc7c24a1364cf9afee48b5a8c8dcc9
Lucas Gomes <contact@lucasgomes.tech>
2021-01-23 20:16:48 -0300

    feat: allow multiple documents and relationships

--- src/ddl.sql
+++ src/ddl.sql
@@ -6,13 +6,12 @@ CREATE TYPE "gender_t" AS ENUM (
   'ANONYMOUS'
 );
 
-CREATE TYPE "person_national_id_t" AS ENUM (
+CREATE TYPE "document_t" AS ENUM (
   'RG',
-  'CPF'
-);
-
-CREATE TYPE "company_national_id_t" AS ENUM (
-  'CNPJ'
+  'CPF',
+  'CNPJ',
+  'PASSPORT',
+  'OTHER'
 );
 
 CREATE TYPE "device_t" AS ENUM (
@@ -20,58 +19,74 @@ CREATE TYPE "device_t" AS ENUM (
   'TABLET'
 );
 
+CREATE TYPE "relationship_t" AS ENUM (
+  'ADMIN',
+  'STUDENT',
+  'EMPLOYEE',
+  'MEMBER',
+  'MODERATOR',
+  'COLABORATOR'
+);
+
 /* TABLES */
 CREATE TABLE "t_person" (
-  "id" int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  "national_id" varchar,
-  "national_id_type" "person_national_id_t",
+  "id" SERIAL PRIMARY KEY,
   "name" varchar NOT NULL,
   "birth_date" date,
-  "gender" "gender_t" NOT NULL,
-  "email" varchar UNIQUE NOT NULL,
+  "gender" gender_t NOT NULL,
+  "email" varchar UNIQUE,
+  "country_id" int,
+  "modified_at" timestamptz DEFAULT (now()),
+  "created_at" timestamptz DEFAULT (now())
+);
+
+CREATE TABLE "t_document" (
+  "id" SERIAL PRIMARY KEY,
+  "owner_id" int,
+  "document_id" varchar,
+  "document_type" document_t,
   "country_id" int,
   "modified_at" timestamptz DEFAULT (now()),
   "created_at" timestamptz DEFAULT (now())
 );
 
 CREATE TABLE "t_company" (
-  "id" int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  "national_id" varchar,
-  "national_id_type" "company_national_id_t",
+  "id" SERIAL PRIMARY KEY,
   "country_id" int,
   "primary_contact" int,
   "name" varchar NOT NULL,
-  "email" varchar UNIQUE NOT NULL,
+  "email" varchar UNIQUE,
   "modified_at" timestamptz DEFAULT (now()),
   "created_at" timestamptz DEFAULT (now())
 );
 
 CREATE TABLE "t_country" (
-  "id" int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  "id" SERIAL PRIMARY KEY,
   "code" int UNIQUE,
   "name" varchar NOT NULL
 );
 
-CREATE TABLE "t_student" (
-  "id" int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  "person_id" int,
-  "company_id" int,
-  "student_code" varchar NOT NULL,
+CREATE TABLE "t_group" (
+  "id" SERIAL PRIMARY KEY,
+  "owner_id" int,
+  "name" varchar NOT NULL,
+  "goal" varchar NOT NULL,
   "modified_at" timestamptz DEFAULT (now()),
   "created_at" timestamptz DEFAULT (now())
 );
 
-CREATE TABLE "t_company_employee" (
-  "id" int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  "person_id" int,
-  "company_id" int,
-  "employee_code" varchar NOT NULL,
+CREATE TABLE "t_relationship" (
+  "id" SERIAL PRIMARY KEY,
+  "target_id" int,
+  "source_id" int,
+  "relationship_type" relationship_t NOT NULL,
+  "code" varchar,
   "modified_at" timestamptz DEFAULT (now()),
   "created_at" timestamptz DEFAULT (now())
 );
 
 CREATE TABLE "t_device" (
-  "id" int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  "id" SERIAL PRIMARY KEY,
   "person_id" int,
   "number" int,
   "imei" int UNIQUE,
@@ -81,45 +96,65 @@ CREATE TABLE "t_device" (
   "created_at" timestamptz DEFAULT (now())
 );
 
+CREATE TABLE "t_study_guide" (
+  "id" SERIAL PRIMARY KEY,
+  "owner_id" int,
+  "title" varchar NOT NULL,
+  "description" varchar NOT NULL,
+  "goal" varchar NOT NULL,
+  "activities" int NOT NULL,
+  "duration" int NOT NULL,
+  "modules" int NOT NULL,
+  "used_by" int NOT NULL,
+  "modified_at" timestamptz DEFAULT (now()),
+  "created_at" timestamptz DEFAULT (now())
+);
+
 /* CONSTRAINTS */
 ALTER TABLE "t_person" ADD FOREIGN KEY ("country_id") REFERENCES "t_country" ("id");
 
+ALTER TABLE "t_document" ADD FOREIGN KEY ("owner_id") REFERENCES "t_person" ("id");
+
+ALTER TABLE "t_document" ADD FOREIGN KEY ("owner_id") REFERENCES "t_company" ("id");
+
+ALTER TABLE "t_document" ADD FOREIGN KEY ("country_id") REFERENCES "t_country" ("id");
+
 ALTER TABLE "t_company" ADD FOREIGN KEY ("country_id") REFERENCES "t_country" ("id");
 
-ALTER TABLE "t_company" ADD FOREIGN KEY ("primary_contact") REFERENCES "t_company_employee" ("id");
+ALTER TABLE "t_company" ADD FOREIGN KEY ("primary_contact") REFERENCES "t_person" ("id");
 
-ALTER TABLE "t_student" ADD FOREIGN KEY ("person_id") REFERENCES "t_person" ("id");
+ALTER TABLE "t_group" ADD FOREIGN KEY ("owner_id") REFERENCES "t_person" ("id");
 
-ALTER TABLE "t_student" ADD FOREIGN KEY ("company_id") REFERENCES "t_company" ("id");
+ALTER TABLE "t_group" ADD FOREIGN KEY ("owner_id") REFERENCES "t_company" ("id");
 
-ALTER TABLE "t_company_employee" ADD FOREIGN KEY ("person_id") REFERENCES "t_person" ("id");
+ALTER TABLE "t_relationship" ADD FOREIGN KEY ("target_id") REFERENCES "t_person" ("id");
 
-ALTER TABLE "t_company_employee" ADD FOREIGN KEY ("company_id") REFERENCES "t_company" ("id");
+ALTER TABLE "t_relationship" ADD FOREIGN KEY ("target_id") REFERENCES "t_company" ("id");
+
+ALTER TABLE "t_relationship" ADD FOREIGN KEY ("source_id") REFERENCES "t_person" ("id");
+
+ALTER TABLE "t_relationship" ADD FOREIGN KEY ("source_id") REFERENCES "t_company" ("id");
+
+ALTER TABLE "t_relationship" ADD FOREIGN KEY ("source_id") REFERENCES "t_group" ("id");
 
 ALTER TABLE "t_device" ADD FOREIGN KEY ("person_id") REFERENCES "t_person" ("id");
 
-CREATE UNIQUE INDEX  
-ON "t_person" ("national_id","national_id_type");
+ALTER TABLE "t_study_guide" ADD FOREIGN KEY ("owner_id") REFERENCES "t_person" ("id");
 
-CREATE UNIQUE INDEX  
-ON "t_company" ("national_id","national_id_type");
+CREATE UNIQUE INDEX ON "t_document" ("document_id", "document_type");
 
-CREATE UNIQUE INDEX  
-ON "t_student" ("person_id","company_id","student_code");
-
-CREATE UNIQUE INDEX  
-ON "t_company_employee" ("person_id","company_id","employee_code");
+CREATE UNIQUE INDEX ON "t_relationship" ("target_id", "source_id");
 
 /* PROCEDURES */
-CREATE OR REPLACE PROCEDURE create_person(VARCHAR, "person_national_id_t", VARCHAR, "gender_t", VARCHAR, INT, TIMESTAMPTZ, INOUT id INT)
+CREATE OR REPLACE PROCEDURE create_person(VARCHAR, "gender_t", VARCHAR, INT, TIMESTAMPTZ, INOUT id INT)
 LANGUAGE plpgsql    
 AS $$
 BEGIN
  
     INSERT INTO "public"."t_person" 
-      ("national_id", "national_id_type", "name", "gender", "email", "country_id", "birth_date") 
+      ("name", "gender", "email", "country_id", "birth_date") 
     VALUES 
-      ($1, $2, $3, $4, $5, $6, $7) 
+      ($1, $2, $3, $4, $5) 
 	  RETURNING "public"."t_person"."id" INTO id;
  
     COMMIT;
@@ -133,8 +168,6 @@ AS $$
 BEGIN
     RETURN QUERY SELECT 
 		"tp"."id", 
-		"tp"."national_id", 
-		"tp"."national_id_type", 
 		"tp"."name", 
 		"tp"."birth_date", 
 		"tp"."gender", 
@@ -150,37 +183,12 @@ END;
 $$
 LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION search_person_by_national_id(person_national_id VARCHAR)
-RETURNS SETOF "public"."t_person"
-AS $$
-BEGIN
-    RETURN QUERY SELECT 
-		"tp"."id", 
-		"tp"."national_id", 
-		"tp"."national_id_type", 
-		"tp"."name", 
-		"tp"."birth_date", 
-		"tp"."gender", 
-		"tp"."email", 
-		"tp"."country_id", 
-		"tp"."modified_at", 
-		"tp"."created_at" 
-	FROM 
-		"public"."t_person" "tp"
-	WHERE 
-		"tp"."national_id" = person_national_id;
-END;
-$$
-LANGUAGE plpgsql;
-
 CREATE OR REPLACE FUNCTION search_person_by_email(person_email VARCHAR)
 RETURNS SETOF "public"."t_person"
 AS $$
 BEGIN
     RETURN QUERY SELECT 
 		"tp"."id", 
-		"tp"."national_id", 
-		"tp"."national_id_type", 
 		"tp"."name", 
 		"tp"."birth_date", 
 		"tp"."gender", 
--- src/dml.sql
+++ src/dml.sql
@@ -6,7 +6,7 @@ INSERT INTO "public"."t_country" ("code", "name") VALUES (55, 'BRAZIL');
 INSERT INTO "public"."t_country" ("code", "name") VALUES (91, 'INDIA');
 
 /* t_person */
-CALL create_person('308177472', 'RG', 'Carlos Gael Luan da Cruz', 'MALE', 'carlos@email.com', 1, null, 0);
-CALL create_person('92445663890', 'CPF', 'André Marcos Castro', 'MALE', 'andre@email.com', 1, null, 0);
-CALL create_person('164639123', 'RG', 'Tânia Rosângela Rezende', 'FEMALE', 'tania@email.com', 1, null, 0);
-CALL create_person('07415944801', 'CPF', 'Eliane Gabriela Patrícia Silveira', 'ANONYMOUS', 'eliane@email.com', 1, null, 0);
+CALL create_person('Carlos Gael Luan da Cruz', 'MALE', 'carlos@email.com', 1, null, 0);
+CALL create_person('André Marcos Castro', 'MALE', 'andre@email.com', 1, null, 0);
+CALL create_person('Tânia Rosângela Rezende', 'FEMALE', 'tania@email.com', 1, null, 0);
+CALL create_person('Eliane Gabriela Patrícia Silveira', 'ANONYMOUS', 'eliane@email.com', 1, null, 0);
